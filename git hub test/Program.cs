851cfe7105e2f479130fca3bce99e2b69c999051
Michael Beddow <21234436@shrewsbury.ac.uk>
2022-07-13 10:06:59 +0100

    Add project files.

---
+++ git hub test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_hub_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
